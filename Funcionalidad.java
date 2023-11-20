76a0cec0ce1a5ae78a98008526e1092f7b947038
drakopablodev <135961183+drakopablodev@users.noreply.github.com>
2023-11-20 10:31:57 +0100

    Update Funcionalidad.java

--- Funcionalidad.java
+++ Funcionalidad.java
@@ -17,14 +17,15 @@
  * i) stoI : String to Integer
  * j) stoC : String to JSON [TODO]
  * k) ? [TODO]
- * l) stoL : String to List [TODO]
+ * l) stoL : String to Long [TODO]
+ * ll) stoLL : String to List [TODO]
  * m) stoM : String to Map [TODO]
  * n) stoN : String to Node [TODO]
  * o) stoO : String to Object [TODO]
  * p) stoP : String to Predicate [TODO]
  * q) stoQ : String to Query [TODO]
  * r) ? [TODO]
- * s) ? [TODO]
+ * s) stoS : String to Set [TODO]
  * t) stoT : String to Type <type> [TODO]
  * u) stoU : String to Unicode [TODO]
  * v) stoV : String to Vector [TODO]
@@ -35,29 +36,16 @@
  * 
  * @author drakopablo.dev
  */
-public abstract class Funcionalidad {  
-    
-    /**
-     * Método para transformar String a Integer
-     * 
-     * @param String _integer
-     * @return Integer
-     */
-    public static Integer stoI(String _integer) {
-        Integer result = null;
-        try {
-            result = Integer.parseInt(_integer);
-        } catch (Exception e) {
-            // Nada
-        }
-        return result;
-    }
+public abstract class Funcionalidad {
+    /////////////
+    ///// B /////
+    /////////////
     
     /**
      * Método para transformar String a Boolean
      * 
      * @param String _boolean
-     * @return Boolean
+     * @return Boolean or null
      */
     public static Boolean stoB(String _boolean) {
         Boolean result = null;
@@ -68,7 +56,28 @@ public abstract class Funcionalidad {
         }
         return result;
     }
-        
+
+    /**
+     * Método para transformar String a Boolean, a modo coalesce
+     * 
+     * @param String _boolean
+     * @param int _default
+     * @return Boolean or _default
+     */    
+    public static Boolean stoB(String _boolean, boolean _default) {
+        Boolean result = null;
+        try {
+            result = Boolean.parseBoolean(_boolean);
+        } catch (Exception e) {
+            result = _default;
+        }
+        return result;
+    }
+    
+    /////////////
+    ///// D /////
+    /////////////
+    
     /**
      * Método para transformar String a Double
      * 
@@ -86,6 +95,27 @@ public abstract class Funcionalidad {
     }
     
     /**
+     * Método para transformar String a Double, a modo coalesce
+     * 
+     * @param String _double
+     * @param double _default
+     * @return Double or _default
+     */
+    public static Double stoD(String _double, double _default) {
+        Double result = null;
+        try {
+            result = Double.parseDouble(_double);
+        } catch (Exception e) {
+            result = _default;
+        }
+        return result;
+    }
+    
+    /////////////
+    ///// F /////
+    /////////////
+    
+    /**
      * Método para transformar String a Float
      * 
      * @param String _double
@@ -100,6 +130,64 @@ public abstract class Funcionalidad {
         }
         return result;
     }
+    
+    /**
+     * Método para transformar String a Float, a modo coalesce
+     * 
+     * @param String _float
+     * @param double _default
+     * @return Float or _default
+     */
+    public static Double stoF(String _float, double _default) {
+        Double result = null;
+        try {
+            result = Double.parseDouble(_float);
+        } catch (Exception e) {
+            result = _default;
+        }
+        return result;
+    }
+    
+    /////////////
+    ///// I /////
+    /////////////
+    
+    /**
+     * Método para transformar String a Integer
+     * 
+     * @param String _integer
+     * @return Integer or null
+     */
+    public static Integer stoI(String _integer) {
+        Integer result = null;
+        try {
+            result = Integer.parseInt(_integer);
+        } catch (Exception e) {
+            // Nada
+        }
+        return result;
+    }
+    
+    /**
+     * Método para transformar String a Integer, a modo coalesce
+     * 
+     * @param String _integer
+     * @param int _default
+     * @return Integer or _default
+     */
+    public static Integer stoI(String _integer, int _default) {
+        Integer result = null;
+        try {
+            result = Integer.parseInt(_integer);
+        } catch (Exception e) {
+            result = _default;
+        }
+        return result;
+    }
+    
+    /////////////
+    ///// * /////
+    /////////////
     
     /**
      * Método para redondear un Double con "decimals" decimales
